484c361aeab45d4eacf1a5416d668f8a5e6aac33
sachipatel15 <56087840+sachipatel15@users.noreply.github.com>
2019-10-07 14:03:23 -0400

    Add files via upload

---
+++ scripts/object/new-game_save_world.js
@@ -0,0 +1,4 @@
+var rotate = require( "../factory/rotate" );
+var tween = require( "../lib/tween" );
+
+exports = rotate.create("images/new-game_save_world.png", 244, 231, 195, 195, 1e-5, tween.exponential.co, 500);
